048adffd4b72a2d94990b4699ce92a447474b600
Eяw!и Gontier <ierwinx@me.com>
2022-09-14 18:49:36 -0500

    Initial Commit

---
+++ Pagos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pagos
+//
+//  Created by Erwin Luz León on 14/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
