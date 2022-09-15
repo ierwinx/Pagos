0b176ee9d2da580593f78d71c144f3da072d4d87
Eяw!и Gontier <ierwinx@me.com>
2022-09-14 19:46:48 -0500

    apple pay in swift

--- Pagos/ViewController.swift
+++ Pagos/ViewController.swift
@@ -1,19 +1,40 @@
-//
-//  ViewController.swift
-//  Pagos
-//
-//  Created by Erwin Luz León on 14/09/22.
-//
-
 import UIKit
+import PassKit
 
 class ViewController: UIViewController {
-
+    
+    private var paymentRequest = PKPaymentRequest()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        paymentRequest.merchantIdentifier = "merchant.com.bugsoft.pay"
+        paymentRequest.supportedNetworks = [.masterCard, .visa, .amex]
+        paymentRequest.supportedCountries = ["MX", "US"]
+        paymentRequest.merchantCapabilities = .capability3DS
+        paymentRequest.countryCode = "MX"
+        paymentRequest.currencyCode = "MXN"
     }
-
-
+    
+    @IBAction func payAction(_ sender: Any) {
+        // Esto es que compro
+        paymentRequest.paymentSummaryItems = [PKPaymentSummaryItem(label: "Fabuloso iPhone 14 1 PB", amount: 0.01)]
+        
+        if let controllerPago = PKPaymentAuthorizationViewController(paymentRequest: paymentRequest) {
+            controllerPago.delegate = self
+            present(controllerPago, animated: true)
+        }
+    }
+    
 }
 
+extension ViewController: PKPaymentAuthorizationViewControllerDelegate {
+    
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        controller.dismiss(animated: true)
+    }
+    
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
+    }
+    
+}
